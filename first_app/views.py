b7c6b41276f429a2fd818f5b021f3a57e8561012
cksfuf <s3147078@gmail.com>
2024-07-29 10:31:14 +0900

    up

--- first_app/views.py
+++ first_app/views.py
@@ -68,3 +68,19 @@ def posts(request):
     }
 
     return render(request, 'posts.html', context)
+
+
+def ping(request):
+    return render(request, 'ping.html')
+
+
+def pong(request):
+    # request.GET['title']
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+
+    context = {
+        'title': title,
+        'content': content,
+    }
+    return render(request, 'pong.html', context)
